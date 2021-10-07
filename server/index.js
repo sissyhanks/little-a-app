607799708133439a206722586803bc4e71e03b1d
Bernadette McKnight <sissyhanks@yahoo.com>
2021-10-07 09:58:30 -0400

    update serve static file code

--- server/index.js
+++ server/index.js
@@ -57,16 +57,23 @@ app.get('/', (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 
-
+// Serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.resolve(__dirname, "./client/build")))
+  
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')) // relative path
+  })
+}
 
 // ...
 // Right before your app.listen(), add this:
 // Step 1:
-app.use(express.static(path.resolve(__dirname, "./client/build")));
-// Step 2:
-app.get("*", function (request, response) {
-  response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
-});
+// app.use(express.static(path.resolve(__dirname, "./client/build")));
+// // Step 2:
+// app.get("*", function (request, response) {
+//   response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+// });
 
 app.listen(process.env.PORT, console.log(`Server running on port ${PORT}`));
 
