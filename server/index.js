8c312fe56f507d69e10a8a39a180d922a087cbde
Bernadette McKnight <sissyhanks@yahoo.com>
2021-10-07 09:10:56 -0400

    run build process before deployment

--- server/index.js
+++ server/index.js
@@ -1,20 +1,26 @@
 import express from 'express';
 import mongoose from 'mongoose';
-import cors from 'cors';
+
 import cookieParser from 'cookie-parser';
 
 import dotenv from 'dotenv';
+import cors from 'cors';
 import { connectDB } from './config/db.js'
+
+import("./models/user");
+
+connectDB();
+
+import routes from './routes/routes.js'
 
 // ... other imports 
 import  path from 'path';
 
-// ... other app.use middleware 
-app.use(express.static(path.join(__dirname, "client", "build")))
 
 
 
-import routes from './routes/routes.js'
+
+
 
 // mongoose.connect('mongodb://localhost/pharm_assist', {
 //       //things that prevent warnings in console???? 8 &9 gave me shit in the console ??? commented them out are OK
@@ -25,9 +31,12 @@ import routes from './routes/routes.js'
 //     console.log(`Mongo DB connected`);
 //   });
 
+dotenv.config({ path: '.env'});
+
 const app = express();
 
-
+// ... other app.use middleware 
+app.use(express.static(path.join(__dirname, "client", "build")))
 
 app.use(express.urlencoded({
   limit: "30mb",
@@ -47,16 +56,16 @@ app.get('/', (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 
-connectDB();
+
 
 // ...
 // Right before your app.listen(), add this:
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+// Step 1:
+app.use(express.static(path.resolve(__dirname, "./client/build")));
+// Step 2:
+app.get("*", function (request, response) {
+  response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
 });
-
-
 
 app.listen(PORT, console.log(`Server running on port ${PORT}`));
 
-dotenv.config({ path: '.env'});
